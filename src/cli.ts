ef540366970c5fb53f1b4dbc63deecaeba0c8d9d
Neeraj Kashyap <nkashy1@gmail.com>
2023-10-07 02:46:11 -0700

    Started working on CLI
    It will use `commander` to generate the CLI and `terminal-kit` for the
    TUI that the CLI runs.

---
+++ src/cli.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("checkpls")
+  .description("Check, Please CLI")
+  .version("0.1.0")
+  .option("-k, --keyfile <file>", "path to account keyfile")
+  .option("-p, --password <str>", "password for account keyfile")
+  .action((options) => {
+    console.log(options);
+  });
+
+program.parse(process.argv);
